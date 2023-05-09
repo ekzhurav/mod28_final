80c142551d5219e67771b71954eeae2e9f98c3d0
ekzhurav <119040858+ekzhurav@users.noreply.github.com>
2023-05-09 18:57:15 +0300

    Create valid_data.py

---
+++ test_data/valid_data.py
@@ -0,0 +1,7 @@
+# Переменные с валидными  данными для тестов
+
+correct_logins = ['katena_rom@mail.ru', '89096695332']
+correct_pass = 'Q123456q23'
+
+agreement_url = 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html'
+agreement_title = 'Публичная оферта о заключении Пользовательского соглашения на использование Сервиса «Ростелеком ID»'
